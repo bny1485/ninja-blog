476ddb5e75731f4e20d33c9222946eff0829c3cd
bny1485 <benyaminmahmoudyan@gmail.com>
2020-12-13 00:06:34 +0330

    dynamic template

--- articles/views.py
+++ articles/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import Article
 
 # Create your views here.
 def article(request):
-    return render(request, 'article.html')
+    articles = Article.objects.all().order_by('date')
+    return render(request, 'article.html', {'articles': articles})
