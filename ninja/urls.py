bf6fd6fdbaa01d5c72aef40bd2da35333f06748b
bny1485 <benyaminmahmoudyan@gmail.com>
2020-12-12 21:58:06 +0330

    create app

--- ninja/urls.py
+++ ninja/urls.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 from django.urls import path
-from django.conf.urls import url
+from django.conf.urls import url, include
 
 from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    url(r'^articles/', include('articles.urls')),
     path(r'about/', views.about),
     url(r'', views.home),
 ]
