bf9d56474a57e021d2ee4ed8cab1c3f7df81dac4
bny1485 <benyaminmahmoudyan@gmail.com>
2020-12-13 12:59:25 +0330

    use slug

--- articles/urls.py
+++ articles/urls.py
@@ -5,5 +5,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'', views.article),
+    path('', views.article_list),
+    path('<slug>/', views.article_detail),
 ]
