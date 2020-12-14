da172b154407d2ee3572648a4ed473cdaffab696
bny1485 <benyaminmahmoudyan@gmail.com>
2020-12-14 21:38:43 +0330

    Logging Users In

--- accounts/views.py
+++ accounts/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login
 
 
 def signup_views(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
-            form.save()
+            user = form.save()
             # log user in
+            login(request, user)
             return redirect("articles:list_article")
     else:
         form = UserCreationForm()
@@ -20,6 +22,8 @@ def login_views(request):
         form = AuthenticationForm(data=request.POST)
         if form.is_valid():
             # login user
+            user = form.get_user()
+            login(request, user)
             return redirect("articles:list_article")
     else:
         form = AuthenticationForm()
