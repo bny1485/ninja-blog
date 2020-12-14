7699fb29681d73b444a358614defd2c3f259b11a
bny1485 <benyaminmahmoudyan@gmail.com>
2020-12-14 20:19:21 +0330

    saving users

--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 def signup_views(request):
-    form = UserCreationForm()
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            # log user in
+            return redirect("articles:list_article")
+    else:
+        form = UserCreationForm()
     context = {'form': form}
     return render (request, 'accounts/signup.html', context)
