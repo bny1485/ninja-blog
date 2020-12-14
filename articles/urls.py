95765eed840b3f81697610569366891f7f8f0fb9
bny1485 <benyaminmahmoudyan@gmail.com>
2020-12-14 22:34:23 +0330

    create new article

--- articles/urls.py
+++ articles/urls.py
@@ -8,5 +8,6 @@ app_name = "articles"
 
 urlpatterns = [
     path('', views.article_list, name='list_article'),
+    path('create/', views.article_create, name='create_article'),
     path('<slug>/', views.article_detail, name='datail_article'),
 ]
--- articles/views.py
+++ articles/views.py
@@ -6,10 +6,13 @@ from django.http import HttpResponse
 # Create your views here.
 def article_list(request):
     articles = Article.objects.all().order_by('date')
-    return render(request, 'article.html', {'articles': articles})
+    return render(request, 'articles/article.html', {'articles': articles})
 
 
 def article_detail(request, slug):
     article = Article.objects.get(slug=slug)
     context = {'article': article}
-    return render(request, 'article_detail.html', context)
+    return render(request, 'articles/article_detail.html', context)
+
+def article_create(request):
+    return render(request, 'articles/article_create.html')
