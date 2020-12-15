370726fcc1961fa1a36f323728ffd039d4b0b2ce
bny1485 <benyaminmahmoudyan@gmail.com>
2020-12-15 15:06:15 +0330

    Foreign Key

--- articles/models.py
+++ articles/models.py
@@ -1,8 +1,8 @@
 import os
 from django.db import models
+from django.contrib.auth.models import User
 
 
-# Create your models here.
 def get_filename(file_path):
     base_name = os.path.basename(file_path)
     ext = os.path.splitext(base_name)
@@ -20,8 +20,9 @@ class Article(models.Model):
     slug = models.SlugField()
     body = models.TextField()
     date = models.DateTimeField(auto_now_add=True)
-    thumb = models.ImageField(upload_to=image_name, default="default.png", blank=True)
-    # TODO add in author
+    thumb = models.ImageField(upload_to=image_name,
+                              default="default.png", blank=True)
+    author = models.ForeignKey(User, default=None, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
--- articles/views.py
+++ articles/views.py
@@ -22,6 +22,9 @@ def article_create(request):
         form = forms.CreateArticle(request.POST, request.FILES)
         if form.is_valid():
             # save article to data base
+            instance = form.save(commit=False)
+            instance.author = request.user
+            instance.save()
             return redirect("articles:list_article")
     else:
         form = forms.CreateArticle()
