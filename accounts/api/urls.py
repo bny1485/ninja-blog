bb37f0b3e12615e6a490864d8e2f30ada0a606c0
bny1485 <benyaminmahmoudyan@gmail.com>
2021-02-15 23:41:23 +0330

    Generating Authentication Tokens

--- accounts/api/urls.py
+++ accounts/api/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from accounts.api.views import registration_view
 
+from rest_framework.authtoken.views import obtain_auth_token
 
 app_name = 'account'
 
 
 urlpatterns = [
     path('registr', registration_view, name='register'),
+    path('login', obtain_auth_token, name='login'),
 ]
