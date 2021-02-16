2abd36da6f52ed363a1de97d3bdf2f7f237650da
bny1485 <benyaminmahmoudyan@gmail.com>
2021-02-16 20:18:29 +0330

    Additional Fields  SerializerMethodField

--- articles/api/serializers.py
+++ articles/api/serializers.py
@@ -3,6 +3,12 @@ from articles.models import Article
 
 
 class ArticleSerializer(serializers.ModelSerializer):
+    username = serializers.SerializerMethodField('get_username_from_author')
+
     class Meta:
         model = Article
-        fields = ['title', 'slug', 'body', 'thumb']
+        fields = ['title', 'slug', 'body', 'thumb', 'username']
+
+    def get_username_from_author(self, blog_post):
+        username = blog_post.author.username
+        return username
